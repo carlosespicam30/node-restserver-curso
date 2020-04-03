3ed2912749cdcf83d9972415167708ea57ec91fc
Carlos <carlosespicam@gmail.com>
2020-04-03 13:07:49 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -15,10 +15,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-//if (process.env.NODE_ENV === 'dev') {
-//  urlDB = 'mongodb://localhost:27017/cafe';
-//} else {
-urlDB = 'mongodb+srv://carlosespicam:Carbia20130701@cluster0-br4tg.mongodb.net/cafe';
-//}
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://carlosespicam:Carbia20130701@cluster0-br4tg.mongodb.net/cafe';
+}
 
 process.env.URLDB = urlDB;
